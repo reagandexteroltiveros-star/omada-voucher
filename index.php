5d92e918bd77b4bed107797e9561fc43adfca4a4
reagandexteroltiveros-star <reagandexter.oltiveros@gmail.com>
2026-03-01 22:40:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
-
-// =======================
-// CONFIG
-// =======================
-$botToken = getenv('8414483455:AAGs6rmmLdkx-uFCkpx3-9AEpFXEDXxEeX');
+$botToken = getenv('8414483455:AAGs6rmmLdkx-uFCkpx3-9AEpFXEDXxEeXI');
 $chatId   = getenv('5863793961');
 
-// =======================
-// GET DATA FROM OMADA
-// =======================
 $username = $_REQUEST['username'] ?? 'Unknown';
 $ip       = $_REQUEST['userip'] ?? $_SERVER['REMOTE_ADDR'];
 $mac      = $_REQUEST['usermac'] ?? 'Unknown';
@@ -16,9 +9,6 @@ $site     = $_REQUEST['site'] ?? 'Office';
 
 $time = date("Y-m-d H:i:s");
 
-// =======================
-// CREATE TELEGRAM MESSAGE
-// =======================
 $message  = "📶 Omada Voucher Login\n\n";
 $message .= "👤 User: $username\n";
 $message .= "🌐 IP: $ip\n";
@@ -26,9 +16,6 @@ $message .= "🖥 MAC: $mac\n";
 $message .= "🏢 Site: $site\n";
 $message .= "⏰ Time: $time";
 
-// =======================
-// SEND TO TELEGRAM
-// =======================
 $url = "https://api.telegram.org/bot{$botToken}/sendMessage";
 
 $postData = [
@@ -47,7 +34,4 @@ $options = [
 $context = stream_context_create($options);
 file_get_contents($url, false, $context);
 
-// =======================
-// RETURN SUCCESS TO OMADA
-// =======================
 echo "success";
